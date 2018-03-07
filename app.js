a30c46f6d9093f1c2239a701b2afbb827eadb01a
Captain-Australia <37135471+Captain-Australia@users.noreply.github.com>
2018-03-07 17:55:31 +1100

    Cleaned Up

--- app.js
+++ app.js
@@ -73,44 +73,4 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// OLD ORIGINAL OPENSHIFT TEMPLATE
-//let server = http.createServer(function (req, res) {
-//  let url = req.url;
-//  if (url == '/') {
-//    url += 'static/index.html';
-//  }
-//
-//  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-//  //            for OpenShift health monitoring
-//
-//  if (url == '/persist') {
-//    res.writeHead(200);
-//    res.end('Life is good.');
-//  } else if (url == '/health') {
-//    res.writeHead(200);
-//    console.log("Port: "+env.NODE_PORT || 3000);
-//    res.end();
-//  } else if (url == '/info/gen' || url == '/info/poll') {
-//    res.setHeader('Content-Type', 'application/json');
-//    res.setHeader('Cache-Control', 'no-cache, no-store');
-//    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-//  } else {
-//    fs.readFile('./static' + url, function (err, data) {
-//      if (err) {
-//        res.writeHead(404);
-//        res.end('Not found');
-//      } else {
-//        let ext = path.extname(url).slice(1);
-//        res.setHeader('Content-Type', contentTypes[ext]);
-//        if (ext === 'html') {
-//          res.setHeader('Cache-Control', 'no-cache, no-store');
-//        }
-//        res.end(data);
-//      }
-//    });
-//  }
-//});
-//
-//server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-//  console.log(`FvP Application worker ${process.pid} started...`);
-//});
+
